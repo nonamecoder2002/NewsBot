37b965f0c84af1e789d4fa202af5f70e75e0bb0a
nonamecoder2002 <alex2002uz@gmail.com>
2020-01-27 17:54:06 +0200

    added MARKDOWN

--- main.py
+++ main.py
@@ -63,8 +63,9 @@ def get_news(update, context):
     ]
     reply_markup = InlineKeyboardMarkup(keyboard)
     article = parse(article_raw)
-    context.bot.send_photo(chat_id=update.message.chat.id, photo=article[0], caption=article[1] + '\n' + article[2],
-                           reply_markup=reply_markup)
+    caption_ = '*'+article[1]+'* \n' + '[Посилання Тут]('+article[2]+')'
+    context.bot.send_photo(chat_id=update.message.chat.id, photo=article[0], caption=caption_,
+                           reply_markup=reply_markup, parse_mode='MARKDOWN')
     html_global = str(html_global).replace(str(article_raw), '')
 
 
@@ -76,14 +77,15 @@ def list_down(update, context):
     ]
     reply_markup = InlineKeyboardMarkup(keyboard)
     bot.edit_message_caption(chat_id=query.message.chat_id, message_id=query.message.message_id,
-                             caption=query.message.caption)
+                             parse_mode='MARKDOWN', caption=query.message.caption)
+    print(query.message.caption)
     global html_global
     article = BeautifulSoup(html_global, 'html.parser').article
     html_global = str(html_global).replace(str(article), '')
     article = parse(article)
-    bot.send_photo(chat_id=query.message.chat.id, photo=article[0], caption=article[1] + '\n' + article[2],
-                   reply_markup=reply_markup)
-    
+    caption_ = '*' + article[1] + '* \n' + '[inline URL](' + article[2] + ')'
+    bot.send_photo(chat_id=query.message.chat.id, photo=article[0], caption=caption_,
+                   reply_markup=reply_markup, parse_mode='MARKDOWN')
 
 
 def main():

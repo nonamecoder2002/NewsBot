9ba91297f257c7cef18bfe820ce393c506839b46
nonamecoder2002 <alex2002uz@gmail.com>
2020-01-28 22:11:19 +0200

    updated

--- main.py
+++ main.py
@@ -17,12 +17,6 @@ def start(update, context):
 
 
 def parse(bs4_obj):
-    """
-
-    :param bs4_obj: <article class = 'c-entry h-entry'>
-    :return: list, [0] - img_url,
-    [1] - desc for artic, [2] - link
-    """
     article_raw = bs4_obj.a
     article_link = article_raw['href']
     article_img_url = article_raw.img['src']
@@ -31,11 +25,6 @@ def parse(bs4_obj):
 
 
 def req_obj_to_bs4(url_):
-    """
-
-    :param url_: url
-    :return: html ready to parse
-    """
     html_raw = requests.get(url=url_).content
     html_bs4 = BeautifulSoup(html_raw, 'html.parser')
     return html_bs4

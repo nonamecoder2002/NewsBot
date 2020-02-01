7203aeb2be3471765ced11097e7f1cfaf9351c67
andrew000 <11490628+andrew000@users.noreply.github.com>
2020-02-01 23:16:34 +0200

    update from master

--- main.py
+++ main.py
@@ -59,15 +59,15 @@ def get_news(update, context):
     caption_ = '*' + article[1] + '*'
     context.bot.send_photo(chat_id=update.message.chat.id, photo=article[0], caption=caption_,
                            reply_markup=reply_markup, parse_mode=ParseMode.MARKDOWN)
-    html_global = str(html_global).replace(str(article_raw), '')
+    html_global = BeautifulSoup(str(html_global).replace(str(article_raw), ''), 'html.parser')
 
 
 def list_down(update, context):
     bot = context.bot
     query = update.callback_query
     global html_global
-    article = BeautifulSoup(html_global, 'html.parser').article
-    html_global = str(html_global).replace(str(article), '')
+    article = html_global.article
+    html_global = BeautifulSoup(str(html_global).replace(str(article), ''), 'html.parser')
     article = parse(article)
     url_keyboard = InlineKeyboardMarkup([query.message.reply_markup.inline_keyboard[0]])
     bot.edit_message_caption(chat_id=query.message.chat_id, message_id=query.message.message_id,

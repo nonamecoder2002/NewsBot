0c5c467b943915bd69cd12cc5972db65488fd067
nonamecoder2002 <alex2002uz@gmail.com>
2020-02-05 17:46:43 +0200

    refactor start

--- main.py
+++ main.py
@@ -9,91 +9,35 @@ logging.basicConfig(handlers=[logging.FileHandler('log.txt', 'w', 'utf-8')],
                     format='[*] {%(pathname)s:%(lineno)d} %(asctime)s - %(levelname)s - %(message)s')
 logger = logging.getLogger(__name__)
 
-session_string = {}  # {user_id: [articles]}
-
 
 def start(update, context):
 
-    context.bot.send_message(text="Я бот", chat_id=update.message.chat_id)
-
-
-def prepare_session_string(update, url):
-    global session_string
-    html_raw = requests.get(url=url).text
-    html_bs4 = BeautifulSoup(html_raw, 'lxml')
-    article_list = []
-
-    for article in html_bs4.find_all('article'):
-        url = article.div.a['href']
-        img_url = None
-        description = None
-
-        for img in article.find_all('img'):
-            try:
-                if img['class'][0] == 'c-post-img':
-                    img_url = img['src']
-                    description = img['alt']
-                    break
-
-                elif img['class'][0] == 'lazyload c-post-img' or \
-                        img['class'][0] == 'lazyload':
-                    img_url = img['data-src']
-                    description = img['alt']
-                    break
-            except Exception as err:
-                if err == 'class':
-                    img_url = img['src']
-                    description = img['alt']
-                    break
-
-        if img_url is None or description is None:
-            continue
-        article_list.append({'url': url, 'img_url': img_url, 'description': description})
-
-    session_string.update({update.message.from_user.id: article_list})
+    context.bot.send_message(text="Найновіші новини тут!", chat_id=update.message.chat_id)
 
 
 def get_news(update, context):
-    global session_string
-    prepare_session_string(update, 'https://tsn.ua/ukrayina')
-    url = session_string[update.message.from_user.id][0]['url']
-    description = session_string[update.message.from_user.id][0]['description']
-    img_url = session_string[update.message.from_user.id][0]['img_url']
 
     keyboard = InlineKeyboardMarkup([
-        [InlineKeyboardButton(text='Посилання Тут', url=url)],
+        [InlineKeyboardButton(text='Посилання Тут', url='')],
         [InlineKeyboardButton('Далі ⬇️', callback_data='down')]
     ])
-    context.bot.send_photo(chat_id=update.message.chat.id, photo=img_url, caption=f"<b>{description}</b>",
+    context.bot.send_photo(chat_id=update.message.chat.id, photo='', caption='',
                            reply_markup=keyboard, parse_mode=ParseMode.HTML)
-    session_string[update.message.from_user.id].pop(0)
 
 
 def list_down(update, context):
-    global session_string
     query = update.callback_query
-
-    if len(session_string[query.from_user.id]) == 0:
-        query.message.reply_text("Список новин закінчився.\nОновити список: /news")
-        return
-
-    url = session_string[query.from_user.id][0]['url']
-    description = session_string[query.from_user.id][0]['description']
-    img_url = session_string[query.from_user.id][0]['img_url']
-
     keyboard = InlineKeyboardMarkup([
-        [InlineKeyboardButton(text='Посилання Тут', url=url)],
+        [InlineKeyboardButton(text='Посилання Тут', url='')],
         [InlineKeyboardButton('Далі ⬇️', callback_data='down')]
     ])
-    context.bot.send_photo(chat_id=query.message.chat.id, photo=img_url, caption=f"<b>{description}</b>",
-                           reply_markup=keyboard, parse_mode=ParseMode.HTML, timeout=10)
+    context.bot.send_photo(chat_id='', photo='', caption='',
+                           reply_markup=keyboard, parse_mode=ParseMode.HTML, timeout='')
 
     url_keyboard = InlineKeyboardMarkup([query.message.reply_markup.inline_keyboard[0]])
     context.bot.edit_message_caption(chat_id=query.message.chat_id, message_id=query.message.message_id,
                                      parse_mode=ParseMode.HTML, caption=query.message.caption,
                                      reply_markup=url_keyboard, timeout=10)
-
-    session_string[query.from_user.id].pop(0)
 
 
 def error(update, context):

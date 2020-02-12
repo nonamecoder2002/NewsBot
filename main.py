7e7e225c60bac336bbc5d1cf19cb09e3e21be9bf
nonamecoder2002 <alex2002uz@gmail.com>
2020-02-12 19:57:23 +0200

    deploy ready

--- main.py
+++ main.py
@@ -7,7 +7,6 @@ import logging
 from dateutil import parser
 import re
 
-# refactor completed
 logging.basicConfig(handlers=[logging.FileHandler('log.txt', 'w', 'utf-8')],
                     level=logging.INFO,
                     format='[*] {%(pathname)s:%(lineno)d} %(asctime)s - %(levelname)s - %(message)s')
